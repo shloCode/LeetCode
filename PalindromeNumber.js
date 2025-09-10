51d54d741618b63876f1ee4bb9821ceee3e322c7
eaashlo <eaashlo@students.eaaa.dk>
2025-09-10 11:56:15 +0200

    separated out the individual numbers

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -1,17 +1,12 @@
 // dont convert it to a string that is no fun :-)
-function isPalidrome(num) {
-    // use modolo to get the remainder, rec
-    // Count the occourence of each number in each remainder it like
-    // each sub-number minus its predecessor must be modolo 0 with 10*(index+1)
-    var divisor = 10
+function isPalindrome(num) {
+    if (num < 0) return false //The minus is part of the number
     var digits = []
-    while (num > divisor) {
-        var remainder = num % divisor
-        console.log(remainder)
-        digits.push(remainder)
-        divisor = divisor * 10
+    while (num > 0) {
+        digits.push(num % 10)
+        num = Math.floor(num / 10)
     }
     console.log(digits)
 }
 
-isPalidrome(21719)
+isPalindrome(21719)
